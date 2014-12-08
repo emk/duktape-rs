ee5079674724bdaddf01cc18700b7255dbe0ff7d
Eric Kidd <git@randomhacks.net>
2014-12-08 10:14:54 -0500

    Add an example code snippet to the docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,25 @@
 //! Rust interface to Duktape JavaScript interpreter.
+//!
+//! ```
+//! use duktape::{Context,Value,DuktapeResult};
+//!
+//! fn add_example() -> DuktapeResult<Value<'static>> {
+//!     // Create a new JavaScript interpreter.  This will be automatically
+//!     // cleaned up when `ctx` goes out of scope.
+//!     let mut ctx = try!(Context::new());
+//!
+//!     // Load some code from a string.
+//!     try!(ctx.eval("function add(x, y) { return x+y; }"));
+//!
+//!     // Call the function we defined.
+//!     ctx.call("add", &[Value::Number(2.0), Value::Number(1.0)])
+//! }
+//!
+//! assert_eq!(Ok(Value::Number(3.0)), add_example());
+//! ```
+//!
+//! We also have preliminary support for defining JavaScript functions
+//! using Rust, but it's still to ugly to show off.
 
 #![experimental]
 #![feature(globs)]
@@ -9,8 +30,8 @@ extern crate libc;
 extern crate cesu8;
 extern crate "duktape_sys" as ffi;
 
-pub use types::*;
-pub use context::*;
+pub use types::{Value, DuktapeError, DuktapeResult};
+pub use context::{Context, Callback};
 
 mod types;
 mod context;
